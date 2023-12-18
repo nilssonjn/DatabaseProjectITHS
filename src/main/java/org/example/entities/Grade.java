47de6d98bdd624447e9a7ef0a00e5b2babfcf132
jnnilsson <jn.jensnilsson@gmail.com>
2023-12-18 14:15:03 +0100

    first commit, added grade entity. testing to add consolechoice for adding grades to students

---
+++ src/main/java/org/example/entities/Grade.java
@@ -0,0 +1,84 @@
+package org.example.entities;
+
+import jakarta.persistence.*;
+import org.hibernate.proxy.HibernateProxy;
+
+import java.util.Objects;
+
+@Entity
+@Table(name = "Grade", schema = "languageCourseDatabaseTEST")
+public class Grade {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "gradeId", nullable = false)
+    private Integer id;
+
+    @Column(name = "gradeValue", length = 50)
+    private String gradeValue;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "gradeStudentId")
+    private Student gradeStudent;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "gradeTeacherId")
+    private Teacher gradeTeacher;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getGradeValue() {
+        return gradeValue;
+    }
+
+    public void setGradeValue(String gradeValue) {
+        this.gradeValue = gradeValue;
+    }
+
+    public Student getGradeStudent() {
+        return gradeStudent;
+    }
+
+    public void setGradeStudent(Student gradeStudent) {
+        this.gradeStudent = gradeStudent;
+    }
+
+    public Teacher getGradeTeacher() {
+        return gradeTeacher;
+    }
+
+    public void setGradeTeacher(Teacher gradeTeacher) {
+        this.gradeTeacher = gradeTeacher;
+    }
+
+    @Override
+    public String toString() {
+        return "Grade{" +
+                "id=" + id +
+                ", gradeValue='" + gradeValue + '\'' +
+                ", gradeStudent=" + gradeStudent +
+                ", gradeTeacher=" + gradeTeacher +
+                '}';
+    }
+
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null) return false;
+        Class<?> oEffectiveClass = o instanceof HibernateProxy ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass() : o.getClass();
+        Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
+        if (thisEffectiveClass != oEffectiveClass) return false;
+        Grade grade = (Grade) o;
+        return getId() != null && Objects.equals(getId(), grade.getId());
+    }
+
+    @Override
+    public final int hashCode() {
+        return this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
+    }
+}
